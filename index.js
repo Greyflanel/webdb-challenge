52bcf12510be1b1d757cd52d17218db2d0d7eb12
Greyflanel <richardlovelace811@gmail.com>
2019-06-14 12:38:13 -0500

    My second commit. I have made some progress, but I have an error when trying to GET or POST. I am looking at it now.

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const db = require('./data/dbConfig');
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/api/dishes', async (req, res) => {
-    try {
-      const dishes = await db('dishes');
-      res.status(200).json(dishes);
-    } catch (error) {
-      res.status(500).json(error);
-    }
+server.get('/api/projects', (req, res) => {
+    db()
+    .then(allProjects => {
+        res.json(allProjects)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json( { message: 'error retrieving projects'})
+    })
+})
+
+server.post('/api/projects', (req, res) => {
+    db(req.body)
+    .then(newProject => {
+        res.status(201).json(newProject)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: 'error adding new project'})
+    })
 })
 
     const port = 5000;
